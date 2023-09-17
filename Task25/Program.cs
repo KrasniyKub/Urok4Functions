0af8493ad461a27c872996d66b8e021678a132a4
Dmitriy Sereda <nadechke@gmail.com>
2023-09-17 20:16:12 +0300

    Задание 25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа которая принимает на вход два числа - A и B, а затем возводит A в степень B
+int GetNum(string Text)
+{
+    Console.Write($"Введите число {Text}/ ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int AtoThePowerB(int numA, int numB)
+{
+    int result = numA;
+    while(1 < numB)
+    {
+        result = result * numA;
+        numB--;
+    }
+    return result;
+}
+
+int A = GetNum("A");
+int B = GetNum("B");
+Console.WriteLine(AtoThePowerB(A, B));
