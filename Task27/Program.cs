5b9bcb74eb5df920cd510a9f2ab8fb95702dd249
Dmitriy Sereda <nadechke@gmail.com>
2023-09-17 20:16:28 +0300

    Задание 27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа которая принимает на вход цифру и выдает сумму его чисел
+int GetNum()
+{
+    Console.Write("Введите число/ ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int NumbersSum(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number%10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int Num = GetNum();
+Console.WriteLine(NumbersSum(Num));
